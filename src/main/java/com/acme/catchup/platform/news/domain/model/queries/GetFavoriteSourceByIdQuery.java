d41fb846c358e15b6709d8a13bd0083c6a5b1edd
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-05-20 17:13:19 -0500

    feat(favorite-source-queries): added queries support for inbound service.

---
+++ src/main/java/com/acme/catchup/platform/news/domain/model/queries/GetFavoriteSourceByIdQuery.java
@@ -0,0 +1,9 @@
+package com.acme.catchup.platform.news.domain.model.queries;
+
+public record GetFavoriteSourceByIdQuery(Long id) {
+    public GetFavoriteSourceByIdQuery {
+        if (id == null || id < 0) {
+            throw new IllegalArgumentException("id cannot be null or zero.");
+        }
+    }
+}
